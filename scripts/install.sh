716e2094f1a9946ece647e58855bc7389bba13f5
Mohd Ibrahim A. <106313018+mohdibrahimai@users.noreply.github.com>
2026-01-31 13:33:39 +0530

    fix: update installer links to epi-recorder

--- scripts/install.sh
+++ scripts/install.sh
@@ -124,8 +124,8 @@ else
 fi
 
 echo ""
-echo -e "${BLUE}Documentation:${NC} https://github.com/mohdibrahimaiml/EPI-V2.2.0"
-echo -e "${BLUE}Issues:${NC} https://github.com/mohdibrahimaiml/EPI-V2.2.0/issues"
+echo -e "${BLUE}Documentation:${NC} https://github.com/mohdibrahimaiml/epi-recorder"
+echo -e "${BLUE}Issues:${NC} https://github.com/mohdibrahimaiml/epi-recorder/issues"
 
 
  
